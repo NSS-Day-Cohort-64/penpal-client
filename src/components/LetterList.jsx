5a4db606b6fd376d54028a34beb377754396c25e
Steve Brownlee <chortlehoort@gmail.com>
2023-08-01 16:10:10 -0500

    Feetching and displaying letters

--- src/components/LetterList.jsx
+++ src/components/LetterList.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react"
+
 export const LetterList = () => {
+    const [letters, changeLetters] = useState([])
+
+    const fetchLetters = async () => {
+        const response = await fetch("http://localhost:8000/letters", {
+            method: "GET",
+            headers: {
+                "Authorization": `Token ${JSON.parse(localStorage.getItem("penpal_token")).token}`
+            }
+        })
+        const letters = await response.json()
+
+        changeLetters(letters)
+    }
+
+    useEffect(() => {
+        fetchLetters()
+    }, [])
+
     return (
-        <h1>Letter List</h1>
+        <>
+            <h1>Letter List</h1>
+            {
+                letters.map(letter => <div className="border p-5 border-solid hover:bg-violet-700 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">{letter.body}</div>)
+            }
+        </>
     )
 }
